f8f0bdde056fdcf9088dcb0ccc0747f1a3c9512b
Bernard Moy <bernardmoy3@gmail.com>
2026-01-14 20:45:01 +0000

    Fixes again

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
-  basePath: "/Speed-Reader-II",
-  assetPrefix: "/Speed-Reader-II/",
+  // reactStrictMode: true,
+  // basePath: "/Speed-Reader-II",
+  // assetPrefix: "/Speed-Reader-II/",
 };
 
 module.exports = nextConfig;
